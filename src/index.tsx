a128112bfbbf17386181054095504560069e10db
shinima <842351815@qq.com>
2017-09-04 00:57:08 +0800

    temp

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,24 @@
 import './preloaded'
 import * as React from 'react'
-import * as rsc from './rsc'
+import Rsc from './rsc'
 import { range } from './utils'
 import { BrickWall, SteelWall } from './testElements'
 
 const canvasElement = document.querySelector('canvas')
 const ctx = canvasElement.getContext('2d')
 
-rsc.draw(ctx,
+Rsc.draw(
   <g role="brickwall" transform="translate(16, 16)scale(8)">
     {range(8).map(x =>
       range(8).map(y =>
         <BrickWall x={4 * x} y={4 * y} />
       )
     )}
-  </g>
+  </g>,
+  ctx,
 )
 
-rsc.draw(ctx,
+Rsc.draw(
   <g transform="scale(8)">
     <SteelWall x={4} y={4} />
     <SteelWall x={12} y={4} />
@@ -27,5 +28,6 @@ rsc.draw(ctx,
     <SteelWall x={12} y={20} />
     <SteelWall x={20} y={20} />
     <SteelWall x={28} y={20} />
-  </g>
+  </g>,
+  ctx,
 )
--- src/rsc.ts
+++ src/rsc.ts
@@ -1,56 +1,121 @@
 import * as React from 'react'
+import { ComponentClass } from 'react'
 import { parseSvgTransform } from './utils'
 
-export function transformMatrix(ctx: CanvasRenderingContext2D, m: Matrix) {
+type Element = JSX.Element
+type Ctx = CanvasRenderingContext2D
+
+class TopLevelWrapper extends React.Component {
+  render() {
+    return React.Children.only(this.props.children)
+  }
+}
+
+function transformMatrix(ctx: Ctx, m: Matrix) {
   ctx.transform(m.a, m.b, m.c, m.d, m.e, m.f)
 }
 
-export function draw(ctx: CanvasRenderingContext2D, element: JSX.Element) {
-  ctx.save()
-  if (element.type === 'rect') {
-    const props: React.SVGProps<SVGRectElement> = element.props
-    const x = Number(props.x) || 0
-    const y = Number(props.y) || 0
-    const w = Number(props.width)
-    const h = Number(props.height)
-    if (props.fill) {
-      ctx.fillStyle = props.fill
-    }
-    if (props.fill !== 'none') {
-      ctx.fillRect(x, y, w, h)
-    }
-
-    if (props.stroke) {
-      ctx.strokeStyle = props.stroke
-    }
-    if (props.stroke && props.stroke !== 'none') {
-      ctx.strokeRect(x, y, w, h)
-    }
-  } else if (element.type === 'g') {
-    const props: React.SVGProps<SVGGElement> = element.props
-    const svgMatrix = parseSvgTransform(props.transform)
-
-    transformMatrix(ctx, svgMatrix)
-    React.Children.forEach(element.props.children,
-      (child: JSX.Element) => draw(ctx, child))
-  } else if (element.type === 'path') {
-    const props: React.SVGProps<SVGGElement> = element.props
-
-    if (props.fill) {
-      ctx.fillStyle = props.fill
-    }
-    ctx.fill(new Path2D(props.d as any))
-  } else if (typeof element.type === 'function') {
-    if (element.type.prototype instanceof React.Component) {
-      // this.draw(element.type.prototype.render.call(element))
-      // !!!! TODO !!!!
-      draw(ctx, (element as any)._owner._instance.render())
-    } else {
-      // stateless function component
-      draw(ctx, (element.type as React.SFC)(element.props))
-    }
-  } else {
-    console.warn('unknown element', element)
-  }
-  ctx.restore()
+const RscReconciler = {
+  mountComponent(internalInstance: InternalComponent, ctx: Ctx) {
+    return internalInstance.mountComponent(ctx)
+  },
+  // receiveComponent(internalInstance: InternalComponent, nextElement: Element) {
+  //   internalInstance.receiveComponent(nextElement)
+  // },
+  // performUpdateIfNecessary(internalInstance: InternalComponent) {
+  //   internalInstance.performUpdateIfNecessary()
+  // },
 }
+
+interface InternalComponent {
+  _currentElement: Element
+  mountComponent(ctx: Ctx): void
+  /* TODO */ receiveComponent(nextElement: JSX.Element): void
+  /* TODO */ performUpdateIfNecessary(): void
+}
+
+class RscCompositeComponentWrapper implements InternalComponent {
+  _currentElement: Element
+  _renderedComponent: InternalComponent
+  _instance: ComponentClass
+
+  constructor(element: Element) {
+    this._currentElement = element
+  }
+
+  mountComponent(ctx: Ctx) {
+    const Component = this._currentElement.type as ComponentClass
+    const componentInstance = new Component(this._currentElement.props)
+    this._instance
+  }
+
+  receiveComponent(nextElement: Element) { }
+
+  performUpdateIfNecessary() { }
+}
+
+function drawRootComponent(element: JSX.Element, ctx: Ctx) {
+  const wrapperElement = React.createElement(TopLevelWrapper, null, element)
+  const componentInstance = new RscCompositeComponentWrapper(wrapperElement)
+  // const markup = RscReconciler
+}
+
+const Rsc = {
+  draw(element: JSX.Element, ctx: Ctx) {
+    // 直接清除已经绘制的图形
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    // 清除上一次绘制结果之后, 我们直接进行本次绘制
+    drawRootComponent(element, ctx)
+
+    ctx.save()
+    if (element.type === 'rect') {
+      const props: React.SVGProps<SVGRectElement> = element.props
+      const x = Number(props.x) || 0
+      const y = Number(props.y) || 0
+      const w = Number(props.width)
+      const h = Number(props.height)
+      if (props.fill) {
+        ctx.fillStyle = props.fill
+      }
+      if (props.fill !== 'none') {
+        ctx.fillRect(x, y, w, h)
+      }
+
+      if (props.stroke) {
+        ctx.strokeStyle = props.stroke
+      }
+      if (props.stroke && props.stroke !== 'none') {
+        ctx.strokeRect(x, y, w, h)
+      }
+    } else if (element.type === 'g') {
+      const props: React.SVGProps<SVGGElement> = element.props
+      const svgMatrix = parseSvgTransform(props.transform)
+
+      transformMatrix(ctx, svgMatrix)
+      React.Children.forEach(element.props.children,
+        (child: JSX.Element) => Rsc.draw(child, ctx))
+    } else if (element.type === 'path') {
+      const props: React.SVGProps<SVGGElement> = element.props
+
+      if (props.fill) {
+        ctx.fillStyle = props.fill
+      }
+      ctx.fill(new Path2D(props.d as any))
+    } else if (typeof element.type === 'function') {
+      if (element.type.prototype instanceof React.Component) {
+        // this.draw(element.type.prototype.render.call(element))
+        // !!!! TODO !!!!
+        Rsc.draw((element as any)._owner._instance.render(), ctx)
+      } else {
+        // stateless function component
+        Rsc.draw((element.type as React.SFC)(element.props), ctx)
+      }
+    } else {
+      console.warn('unknown element', element)
+    }
+    ctx.restore()
+  },
+}
+
+export default Rsc
+
